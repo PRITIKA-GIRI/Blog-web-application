19984be9a5f0107d097cb23429d722fa7a07ad2b
Pritika Giri <giri.pri003@gmail.com>
2025-05-22 21:33:18 +0545

    loginform and css applied properly to every pages

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 import express from "express"
 import bodyParser from "body-parser"
-
+import dayjs from "dayjs";
 const app=express();
 const port=3000;
 const posts=[];
 const username="Pritika Giri";
 const password="PritikaAdmin";
+const day=dayjs().format('YYYY-MM-DD || dddd ');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
-    res.render("index.ejs",{ posts: posts });
+    
+    res.render("index.ejs",{ posts: posts,today:day ,loginClicked:false,invalid:false});
 })
 
 app.get("/login",(req,res)=>{
-    res.send(`
-        <h2>Login Form</h2>
-         <form method="POST" action="/login">
-            <input type="text" name="username" placeholder="Username" required /><br/>
-            <input type="password" name="password" placeholder="Password" required /><br/>
-            <button type="submit">Login</button>
-        </form>
-        `)  
+    res.render("index.ejs",{invalid:false,posts: posts,today:day,loginClicked:true})
+//    res.render("loginform.ejs",{invalid:false}) ;
 })
+//middleware 
 function authentication(req,res,next){
+    
  if(username===req.body.username && password===req.body.password){
     next();
  }
  else{
-    res.send("Invalid crendentials.")
+    res.render("index.ejs",{invalid:true,loginClicked:true,posts: posts,today:day})
  }
 }
 
@@ -40,9 +38,11 @@ app.post("/login",authentication,(req,res)=>{
 app.get("/create",(req,res)=>{
     res.render("createPost.ejs");
 })
+
 app.get("/about",(req,res)=>{
     res.render("about.ejs");
 })
+
 app.post("/postDetails",(req,res)=>{
     const post={
         authorName:req.body.author,
@@ -54,6 +54,7 @@ app.post("/postDetails",(req,res)=>{
     posts.push(post);
   res.redirect('/');
 })
+
 app.post("/create",(req,res)=>{
     res.render("createPost.ejs");
 })
