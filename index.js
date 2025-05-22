cf5bcc7cf3e9b09c9198ff6c99900b03a2f71727
Pritika Giri <giri.pri003@gmail.com>
2025-05-22 11:38:14 +0545

    added login page

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import bodyParser from "body-parser"
 const app=express();
 const port=3000;
 const posts=[];
+const username="Pritika Giri";
+const password="PritikaAdmin";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -11,6 +13,30 @@ app.use(express.static("public"));
 app.get("/",(req,res)=>{
     res.render("index.ejs",{ posts: posts });
 })
+
+app.get("/login",(req,res)=>{
+    res.send(`
+        <h2>Login Form</h2>
+         <form method="POST" action="/login">
+            <input type="text" name="username" placeholder="Username" required /><br/>
+            <input type="password" name="password" placeholder="Password" required /><br/>
+            <button type="submit">Login</button>
+        </form>
+        `)  
+})
+function authentication(req,res,next){
+ if(username===req.body.username && password===req.body.password){
+    next();
+ }
+ else{
+    res.send("Invalid crendentials.")
+ }
+}
+
+app.post("/login",authentication,(req,res)=>{
+    res.render("adminDashboard.ejs");
+})
+
 app.get("/create",(req,res)=>{
     res.render("createPost.ejs");
 })
@@ -19,6 +45,7 @@ app.get("/about",(req,res)=>{
 })
 app.post("/postDetails",(req,res)=>{
     const post={
+        authorName:req.body.author,
         title:req.body.title,
         content:req.body.content,
         emails:req.body.email
