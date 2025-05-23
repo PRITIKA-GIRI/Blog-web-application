142d27227df04a25906db0668d43514803a1089c
Pritika Giri <giri.pri003@gmail.com>
2025-05-23 12:36:22 +0545

    added further logics and styling,session state

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 import express from "express"
 import bodyParser from "body-parser"
 import dayjs from "dayjs";
+import session from "express-session"
 const app=express();
 const port=3000;
 const posts=[];
 const username="Pritika Giri";
 const password="PritikaAdmin";
+let postId=0;
 const day=dayjs().format('YYYY-MM-DD || dddd ');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.use(session({
+  secret: 'yourSecretKey', // Change this in production
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: null // Session expires when browser closes
+  }
+}));
 
 app.get("/",(req,res)=>{
     
@@ -17,6 +28,15 @@ app.get("/",(req,res)=>{
 })
 
 app.get("/login",(req,res)=>{
+    if(req.session.isAuthenticated){
+        const author = req.session.username;
+        const userPosts = posts.filter(post => post.authorName === author);
+        res.render("adminDashboard.ejs", {
+            posts: userPosts,
+            username: author
+        });
+    }
+    else
     res.render("index.ejs",{invalid:false,posts: posts,today:day,loginClicked:true})
 //    res.render("loginform.ejs",{invalid:false}) ;
 })
@@ -24,6 +44,8 @@ app.get("/login",(req,res)=>{
 function authentication(req,res,next){
     
  if(username===req.body.username && password===req.body.password){
+    req.session.isAuthenticated = true;
+    req.session.username = req.body.username; 
     next();
  }
  else{
@@ -32,7 +54,12 @@ function authentication(req,res,next){
 }
 
 app.post("/login",authentication,(req,res)=>{
-    res.render("adminDashboard.ejs");
+    const author = req.body.username;
+   const userPosts = posts.filter(post => post.authorName === author);
+  res.render("adminDashboard.ejs", {
+    posts: userPosts,
+    username: author
+  });
 })
 
 app.get("/create",(req,res)=>{
@@ -45,6 +72,7 @@ app.get("/about",(req,res)=>{
 
 app.post("/postDetails",(req,res)=>{
     const post={
+        postId:postId++,
         authorName:req.body.author,
         title:req.body.title,
         content:req.body.content,
